401e011dd8012c143eb3e37a2ea420ce31d72aaa
Jihong-Tang <njutangjihong@gmail.com>
2021-06-18 02:25:33 +0800

    update

--- Bioinfo_Contest_2021/Ex2_motif.py
+++ Bioinfo_Contest_2021/Ex2_motif.py
@@ -1,7 +1,26 @@
 import sys
 
+def singleMotif(string, pattern):
+    idx = []
+    for i in range(len(string)):
+        if string[i:i+len(pattern)] == pattern:
+            idx.append(i+1)
+    return idx
+
 def FindMotif(num, string, pattern):
-    
+    res = []
+    if num == 1:
+        res = singleMotif(string, pattern)
+        f = open('./dataset/Ex2_output.txt', 'w')
+        f.write(' '.join(map(str, res)) + '\n')
+        f.close()
+    else:
+        for i in range(num):
+            res.append(singleMotif(string[i], pattern[i]))
+        f = open('./dataset/Ex2_output.txt', 'w')
+        for i in range(num):
+            f.write(' '.join(map(str, res[i])) + '\n')
+        f.close()
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
@@ -18,6 +37,5 @@ if __name__ == '__main__':
         num = 1
         string = 'GATATATGCATATACTT'
         pattern = 'TAT'
-    
     FindMotif(num, string, pattern)
 
